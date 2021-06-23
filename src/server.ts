c358c022af62d235459af1b20010ee90d281f29b
Anderson <andersoninfonet@gmail.com>
2021-06-22 23:22:47 -0300

    🚧 under contsruction

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import './database';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 const port = 3000;
 
